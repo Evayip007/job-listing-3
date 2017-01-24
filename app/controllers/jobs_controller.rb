1859ca94f9be369f442509cc2b52c6d626432137
eva <manfeishi007@163.com>
2017-01-24 16:38:47 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,8 +5,9 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
 end
 
 def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
+
   def new
       @job = Job.new
     end
@@ -46,7 +47,7 @@ def index
     private
 
     def job_params
-   params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
- end
- 
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
+  end
+
 end
